41ffccc88aa00a743ef05bfd1e87f1aa6f87dd4c
Artur Vitt <dekster13@hotmail.com>
2017-11-13 21:46:03 +0100

    ANN_MLP and ANN_MLPTest added to index

---
+++ src/main/java/mlp/ANN_MLP.java
@@ -0,0 +1,33 @@
+package mlp;
+
+public class ANN_MLP {
+	
+	public ANN_MLP() {}
+
+	public ANN_MLP(int[] actualLayerSizes) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static enum ACTIVATION_FUNCTION{SIGMOID, GAUSSIAN}
+
+	public int[] getLayerSizes() {
+		// TODO Auto-generated method stub
+		return new int[1];
+	}
+
+	public Neuron[] getInputLayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Neuron[] getLayer(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Neuron[] getOutputLayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/mlp/ANN_MLPTest.java
@@ -0,0 +1,37 @@
+package mlp;
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ANN_MLPTest {
+	private ANN_MLP sut;
+	
+	@BeforeEach
+	void init() {
+		sut = new ANN_MLP();
+	}
+	
+	@Test
+	void testOnConstructorWithArray() {
+		int[] actualLayerSizes = {2,3,1};
+		sut = new ANN_MLP(actualLayerSizes);
+		actualLayerSizes = sut.getLayerSizes();
+		int[] expectedLayerSizes = {2,3,1};
+		assertArrayEquals(actualLayerSizes,expectedLayerSizes);
+	}
+	
+	@Test
+	void testLengthOfCreatedLayers() {
+		int[] layerSizes = {2,3,1};
+		sut = new ANN_MLP(layerSizes);
+		int[] actualInputLayerSizes = new int[3];
+		actualInputLayerSizes[0] = sut.getInputLayer().length;
+		actualInputLayerSizes[1] = sut.getLayer(1).length;
+		actualInputLayerSizes[2] = sut.getOutputLayer().length;
+		int[] expected =  {2,3,1};
+		assertEquals(actualInputLayerSizes,expected);
+	}
+	
+}
