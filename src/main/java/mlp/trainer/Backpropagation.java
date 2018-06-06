eb3b0b979232b76fbaabca3800f3380f89ea8030
Artur Vitt <artur.vitt49@gmail.com>
2018-06-06 20:38:48 +0200

    RED - tests for functions of statistical evaluation
    functions  - isNoneZeroMean, isVarianceLarge, areInputsCorrelated

---
+++ src/main/java/mlp/trainer/Backpropagation.java
@@ -0,0 +1,23 @@
+package mlp.trainer;
+
+public class Backpropagation {
+
+	public boolean[] isNoneZeroMean(float[][] data) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean[] isLargeVariance(float[][] data, float threshold) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean[] areInputsCorrelated(float[][] data, float threshold) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
