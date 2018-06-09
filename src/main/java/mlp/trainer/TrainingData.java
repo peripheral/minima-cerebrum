1e72301317193f6b8a25a6f566460aa26891b6f7
Artur Vitt <artur.vitt49@gmail.com>
2018-06-09 12:07:39 +0200

    GREEN - methods implemented, test pass
    set/get data, getMeans, getVarainces

--- src/main/java/mlp/trainer/TrainingData.java
+++ src/main/java/mlp/trainer/TrainingData.java
@@ -2,29 +2,55 @@ package mlp.trainer;
 
 public class TrainingData {
 
+	private float[][] data = null;
+	private float[] means = null;
+	private float[] variances;
+
 	public void setData(float[][] data) {
-		// TODO Auto-generated method stub
-		
+		this.data = data;
 	}
 
 	public float[][] getData() {
-		// TODO Auto-generated method stub
-		return null;
+		return data;
 	}
 
 	public float[] getVariances() {
-		// TODO Auto-generated method stub
-		return null;
+		return variances;
 	}
 
 	public float[] getMeans() {
-		// TODO Auto-generated method stub
-		return null;
+		if(data.length == 0) {
+			return null;
+		}else {
+			return means;
+		}
 	}
 
 	public void calculateMeans() {
-		// TODO Auto-generated method stub
-		
+		if(means == null && data !=null) {
+			means = new float[data[0].length];
+		}
+		for(int col = 0 ; col < data.length;col++) {
+			means[col] = 0;
+			for(int row = 0; row < data[0].length;row++) {
+				means[col] = means[col]+data[row][col]/data.length;
+			}
+		}
+	}
+	
+	public void calculateVariances() {
+		if(variances == null && data !=null) {
+			variances = new float[data[0].length];
+		}
+		if(means == null) {
+			calculateMeans();
+		}
+		for(int col = 0 ; col < data[0].length;col++) {
+			variances[col] = 0;
+			for(int row = 0; row < data.length;row++) {
+				variances[col] = (float) (variances[col]+Math.pow(data[row][col]-means[col],2)/data.length);
+			}
+		}
 	}
 
 }
--- src/test/java/mlp/trainer/tests/TrainingDataTest.java
+++ src/test/java/mlp/trainer/tests/TrainingDataTest.java
@@ -37,10 +37,10 @@ public class TrainingDataTest {
 	/**
 	 *Test function get variances
 	 * A1 = {1,2,3}, A2 = {4,5,6}, A3 = {7,100,-50}
-	 * Mean A1 = 2, mean A2 = 5, mean A3 = 28.5; 
-	 * Variance A1 = (1-2)^2+(2-2)^2+(3-2)^2 = 1+0+1 = 2
-	 * Variance A2 = (4-5)^2+(5-5)^2+(6-5)^2 = 1+0+1 = 2
-	 * Variance A3 = ((7-28.5)^2+(100-28.5)^2+(-50-28.5)^2)/3 = 3912.25
+	 * Mean A1 = 2, mean A2 = 5, mean A3 = 19; 
+	 * Variance A1 = ((1-2)^2+(2-2)^2+(3-2)^2)/3 = (1+0+1)/3 = 2/3
+	 * Variance A2 = ((4-5)^2+(5-5)^2+(6-5)^2)/3 = (1+0+1)/3 = 2/3
+	 * Variance A3 = ((7-19)^2+(100-19)^2+(-50-19)^2)/3 = 3822
 	 * standard dev A3 = 62.5479815821
 	 */
 	@Test
@@ -49,7 +49,8 @@ public class TrainingDataTest {
 				{2,5,100},
 				{3,6,-50}};
 		sut.setData(data);
-		float[] expected = {2,2, 3912.25f};
+		sut.calculateVariances();
+		float[] expected = {2/3f,2/3f, 3822f};
 		float[] isVarianceLargeActual = sut.getVariances();
 		assertArrayEquals(expected,isVarianceLargeActual);
 	}
@@ -57,10 +58,10 @@ public class TrainingDataTest {
 	/**
 	 *Test function get means
 	 * A1 = {1,2,3}, A2 = {4,5,6}, A3 = {7,100,-50}
-	 * Mean A1 = 2, mean A2 = 5, mean A3 = 28.5; 
+	 * Mean A1 = 2, mean A2 = 5, mean A3 = 19; 
 	 * Variance A1 = (1-2)^2+(2-2)^2+(3-2)^2 = 1+0+1 = 2
-	 * Variance A2 = (4-5)^2+(5-5)^2+(6-5)^2 = 1+0+1 = 2
-	 * Variance A3 = ((7-28.5)^2+(100-28.5)^2+(-50-28.5)^2)/3 = 3912.25
+	 * Variance A2 = (4-5)^2+(5-5)^2+(6-5)^2 = 1+0+1 = 5
+	 * Variance A3 = ((7-19)^2+(100-28.5)^2+(-50-28.5)^2)/3 = 3912.25
 	 * standard dev A3 = 62.5479815821
 	 */
 	@Test
@@ -69,10 +70,10 @@ public class TrainingDataTest {
 				{2,5,100},
 				{3,6,-50}};
 		sut.setData(data);
-		float[] expected = {2,2, 3912.25f};
+		float[] expected = {2,5, 19f};
 		sut.calculateMeans();
 		float[] isVarianceLargeActual = sut.getMeans();
-		assertArrayEquals(expected,isVarianceLargeActual);
+		assertArrayEquals(expected,isVarianceLargeActual,0.00001f);
 	}
 
 }
