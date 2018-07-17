d1200fa3dcf750492dffbdbfe5b8320a0d5da1a1
Artur Vitt <artur.vitt49@gmail.com>
2018-07-17 16:59:11 +0200

    RED: Gradient Descent- test on function to calc grad of output layer
    Task-Url: https://github.com/peripheral/minima-cerebrum/projects/1#card-11397086

---
+++ src/test/java/mlp/trainer/tests/gradient_descent/GradientDescentTest.java
@@ -0,0 +1,49 @@
+package mlp.trainer.tests.gradient_descent;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import mlp.ANN_MLP.ACTIVATION_FUNCTION;
+import mlp.trainer.Backpropagation.COST_FUNCTION_TYPE;
+import mlp.trainer.gradient_descent.GradientDescent;
+
+public class GradientDescentTest{
+	
+	/**
+	 * System under test
+	 */
+	private GradientDescent sut;
+
+	@BeforeEach
+	void init() {
+		sut = new GradientDescent();
+	}
+	
+	/**
+	 * Test for function that calculates gradient of input to error in output layer
+	 * of the output neuron
+	 * Io - neuron input, E = (Predicted - Required)
+	 * ∂(E)^2/∂I_o = gradient
+	 * (∂E^2/∂I_o) => 2E  - first step of derivation
+	 * (∂f(I_o)/∂I_o) => f'(Io), f(.) - softmax
+	 * 2E * fo'(Io) = gradient
+	 */
+	@Test
+	void testCAlculateGradientOfChangeInSquaredErrorVs() {
+		COST_FUNCTION_TYPE costFType = COST_FUNCTION_TYPE.SQUARED_ERROR;
+		ACTIVATION_FUNCTION outputActivationFunction = ACTIVATION_FUNCTION.SOFTMAX;
+		float a = 1;
+		float b = 1;
+		float[] Io = {3f,4f,6f};
+		int neuronIdx = 1;
+		float error = 0.5f;
+		/* Derivative of softmax  ((e^4)((e^3)+(e^4)+(e^6)) - (e^4)(e^4))/((e^3)+(e^4)+(e^6))^2 = 0.10115465582 */
+		float softmaxDerivative = 0.10115465582f;
+		/* Gradient 2 * E * derivative =  2 * 0.5 * 0.10115465582 = 0.10115465582  */
+		float expected = 2 * error * softmaxDerivative;
+		float actual = sut.calculateGradientInputOverError(costFType, outputActivationFunction, error, Io[neuronIdx], a, b);
+		assertEquals(expected,actual);
+	}
+}
